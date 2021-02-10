6ae00737b2a92fb547e32bfeeb861b6cbc4dc95d
adhamsulton <adhamsulton@gmail.com>
2021-02-10 08:57:25 +0500

    proxy

--- src/app/shared/services/book.service.ts
+++ src/app/shared/services/book.service.ts
@@ -4,14 +4,12 @@ import { Observable } from 'rxjs';
 import { Author, Book, Genre } from '../models/book';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class BookService {
+  private baseUrl = 'http://localhost:4200/books';
 
-  private baseUrl = 'http://localhost:8080/books';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getBookList(): Observable<Book[]> {
     return this.http.get<Book[]>(`${this.baseUrl}`);
@@ -33,12 +31,11 @@ export class BookService {
     return this.http.post(`${this.baseUrl}/save`, value);
   }
 
-
   getGenreList(): Observable<Genre[]> {
     return this.http.get<Genre[]>(`${this.baseUrl}/genres`);
   }
 
   getAuthorsList(): Observable<Author[]> {
-    return this.http.get<Author[]>(`${this.baseUrl}/authors`)
+    return this.http.get<Author[]>(`${this.baseUrl}/authors`);
   }
-}                                           
+}
