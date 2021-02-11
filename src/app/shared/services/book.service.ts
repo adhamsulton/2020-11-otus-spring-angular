756110ae200835902346a90666da1ca20e16df2f
adhamsulton <adhamsulton@gmail.com>
2021-02-11 09:55:05 +0500

    review

--- src/app/shared/services/book.service.ts
+++ src/app/shared/services/book.service.ts
@@ -7,28 +7,28 @@ import { Author, Book, Genre } from '../models/book';
   providedIn: 'root',
 })
 export class BookService {
-  private baseUrl = 'http://localhost:4200/books';
+  private baseUrl = 'http://localhost:4200';
 
   constructor(private http: HttpClient) {}
 
   getBookList(): Observable<Book[]> {
-    return this.http.get<Book[]>(`${this.baseUrl}`);
+    return this.http.get<Book[]>(`${this.baseUrl}/books`);
   }
 
   createBook(book: Book): Observable<Book> {
-    return this.http.post<Book>(`${this.baseUrl}` + '/save', book);
+    return this.http.post<Book>(`${this.baseUrl}` + '/books', book);
   }
 
   deleteBook(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`);
+    return this.http.delete(`${this.baseUrl}/books/${id}`);
   }
 
   getBook(id: number): Observable<Book> {
-    return this.http.get<Book>(`${this.baseUrl}/${id}`);
+    return this.http.get<Book>(`${this.baseUrl}/books/${id}`);
   }
 
   updateBook(value: any): Observable<Object> {
-    return this.http.post(`${this.baseUrl}/save`, value);
+    return this.http.post(`${this.baseUrl}/books`, value);
   }
 
   getGenreList(): Observable<Genre[]> {
